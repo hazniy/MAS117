3e100f3909135ac9ecb4bbb6d41b6f4e24b546db
Farhazni Najwa <141621627+hazniy@users.noreply.github.com>
2024-10-07 15:35:41 +0100

    Update Week2.py

--- Week2.py
+++ Week2.py
@@ -1,20 +1,20 @@
 #python variable types 
 approximation = 175.38
-type(approximation) #type to find out type of variable 
+print(type(approximation)) #type to find out type of variable 
 secret_password = "dead parrot"
-type(secret_password)
+print(type(secret_password))
 
 #integer type 
 a = 7
 b = 2
-print(a * b, a + b, a - b, -a, a**b)
+print('product', a * b, 'sum', a + b, 'subtract', a - b, 'negative', -a, 'power of', a**b)
 print(max(a, b), min(a, b)) #max and min
 print("7/3 is", 7 // 3, "with remainder", 7 % 3) #integer division // & remainder %
 
 #exercise 2.1
 #How many digits are there in 2^100?
 formula1 = 2**100 
-print ("there are", len(formula1))
+print ("there are", len(str(formula1)))
 
 #What is the remainder when 3^11 is divided by 11?
 formula2 = (3**11)//11 
@@ -23,7 +23,7 @@ print(formula2)
 #floating point type 
 a = 7.2
 b = 2.5
-print(a * b, a + b, a - b, -a, a**b)
+print('product', a * b, 'sum', a + b,'subtract', a - b, 'negative', -a, 'power of' ,a**b)
 print(max(a, b), min(a,b))
 
 #turn float into integer
@@ -40,13 +40,16 @@ a = -7.8
 b = int(a) 
 print(b)
 
-#celcius to fahrenheit 
-temperature_in_C = float(input("Temperature in Celsius: "))
-temperature_in_F = correct_expression
-print("Temperature in Fahrenheit is", temperature_in_F)
-#try input 37C 
-
 #exercise 2.3 (BMI program) 
-height = input(float("enter your height in metres"))
-          
-               
+height = float(input("enter your height in metres "))
+weight = float(input("enter your weight in kilogram "))
+BMI = weight/height**2
+print("%.1f"% BMI) #round of to 1 place 
+
+#exercise 2.4 
+number = int(input("enter a number: "))
+if number == 0 :
+    print("it is False that your number is non-zero")
+else:
+    print("it is True that your number is non-zero")
+    
