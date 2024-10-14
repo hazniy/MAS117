efb4ab05e9d2718846dad95d7432fd466c1b66a9
Farhazni Najwa <141621627+hazniy@users.noreply.github.com>
2024-10-14 15:24:28 +0100

    Create Hamming Distance.py

---
+++ Hamming Distance.py
@@ -0,0 +1,5 @@
+#hamming distance : between 2 words of same size and total of diff words
+str1 = str(input("enter the first string: "))
+str2 = str(input("enter the second string: "))
+while len(str1) == len(str2): 
+    letter1 = str1, 1
