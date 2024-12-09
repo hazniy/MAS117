a3def0a50a7df6b97192db9555242f6f81e5d5aa
Farhazni Najwa <141621627+hazniy@users.noreply.github.com>
2024-12-10 06:54:34 +0800

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -62,24 +62,60 @@ for i in range(NO_OF_SPOKES):
 plt.gca().set_aspect("equal")
 plt.axis([-1, 9, -1, 9])
 plt.show()
-##########################################################################################################################
 
-import numpy as np 
+#circle with another circle inside
+import numpy as np
 import matplotlib.pyplot as plt
-
-def draw_circle(centre, radius): 
+def draw_circle(centre, radius):
     """Draw a black circle with specified centre and radius."""
     theta = np.linspace(0, 2*np.pi, 100)
     plt.plot(radius*np.cos(theta) + centre[0],
-             radius*np.sin(theta) + centre[1],
-             color="black")
-    
+            radius*np.sin(theta) + centre[1],
+            color="black")
+
 draw_circle([0, 0], 5)
 draw_circle([0, 0], 1)
 draw_circle([3, 0], 2)
 draw_circle([-3, 0], 2)
 draw_circle([0, 3], 2)
 draw_circle([0, -3], 2)
-
 plt.gca().set_aspect("equal")
-plt.show
+plt.show()
+
+#exercise 11.3 spider web
+import numpy as np
+import matplotlib.pyplot as plt
+
+def draw_circle(centre, radius):
+    """Draw a black circle with specified centre and radius."""
+    theta = np.linspace(0, 2 * np.pi, 100)
+    plt.plot(
+        radius * np.cos(theta) + centre[0],
+        radius * np.sin(theta) + centre[1],
+        color="black"
+    )
+
+# Parameters
+NO_OF_SPOKES = 17  # Number of spokes
+NO_OF_CIRCLES = 9  # Number of concentric circles
+MAX_RADIUS = 1.0   # Maximum radius of the outermost circle
+
+# Draw concentric circles
+for i in range(1, NO_OF_CIRCLES + 1):
+    radius = MAX_RADIUS * i / NO_OF_CIRCLES
+    draw_circle([0, 0], radius)
+
+# Draw spokes
+for i in range(NO_OF_SPOKES):
+    phi = i * 2 * np.pi / NO_OF_SPOKES
+    x = MAX_RADIUS * np.cos(phi)
+    y = MAX_RADIUS * np.sin(phi)
+    plt.plot([0, x], [0, y], color="pink")
+
+# Adjust plot settings
+plt.axis("equal")  # Ensure equal aspect ratio
+plt.axis([-1.1, 1.1, -1.1, 1.1])  # Extend axis limits slightly for visibility
+plt.show()
+
+#save picture png
+plt.savefig("picture.png")
