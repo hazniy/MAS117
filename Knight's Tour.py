c8cf67a82177b672227a5fee1e3a08a122d39c67
Farhazni Najwa <141621627+hazniy@users.noreply.github.com>
2025-01-28 05:13:39 +0800

    Create Knight's Tour.py

---
+++ Knight's Tour.py
@@ -0,0 +1,21 @@
+#introduction of knight tours
+#This "game" is basically an implementation of Knight's Tour problem.
+#You have to produce the longest possible sequence of moves of a
+#chess knight, while visiting squares on the board only once.
+#This sequence is called "tour". If your tour visits every square,
+#then you have achieved a full tour. If you have achieved a full tour
+#and from your last position you could move to your initial square,
+#then you have achieved a closed full tour.
+#Currently occupied square is highlighted in pale blue,
+# while possible moves are shown with pale green. Click on the
+# currently occupied square to undo.
+
+#chessboard
+#one knight
+#the next possible move
+#move until no more possible move
+#total move
+#maths behind it
+
+#https://www.youtube.com/watch?v=X-e0jk4I938&t=332s
+#https://www.maths-resources.com/knights/
